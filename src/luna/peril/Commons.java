7c30143e797e273bbfa6318fa7468e36ab8a846b
Eric Rodriguez <ericrod.1998@gmail.com>
2021-08-11 22:11:01 -0700

    Updated Continents enum Continents enum now holds total continent territories

--- src/luna/peril/Commons.java
+++ src/luna/peril/Commons.java
@@ -35,17 +35,17 @@ public class Commons
 	 */
 	public enum Continents
 	{
-		NA("North America", 5), 
-		SA("South America", 2), 
-		EU("Europe", 5), 
-		AF("Africa", 3), 
-		AS("Asia", 7),
-		AU("Australia", 2);
+		NA("North America", 9, 5),
+		SA("South America", 4, 2),
+		EU("Europe", 7, 5),
+		AF("Africa", 6, 3),
+		AS("Asia", 12, 7),
+		AU("Australia", 4, 2);
 		
 		public final String NAME;
 		public final int    BONUS;
 		
-		Continents(String name, int bonus)
+		Continents(String name, int territories, int bonus)
 		{
 			NAME = name;
 			BONUS = bonus;
@@ -85,33 +85,32 @@ public class Commons
 		SOUTHERN_EU(19, "Southern Europe", List.of(15, 17, 18, 20, 21, 35), Continents.EU),
 		
 		// Africa
-		NORTH_AF(20, "Northern Africa",List.of(), Continents.AF),
-		EGYPT(21, "Egypt",List.of(19,20,22,35), Continents.AF),
-		EAST_AF(22, "East Africa",List.of(20,21,23,24,25,35), Continents.AF),
-		CENTRAL_AF(23, "Central Africa",List.of(20,22,24), Continents.AF),
-		SOUTH_AF(24, "South Africa",List.of(22,23,25), Continents.AF),
-		MADAGASCAR(25, "Madagascar",List.of(22,24), Continents.AF),
+		NORTH_AF(20, "Northern Africa", List.of(), Continents.AF),
+		EGYPT(21, "Egypt", List.of(19, 20, 22, 35), Continents.AF),
+		EAST_AF(22, "East Africa", List.of(20, 21, 23, 24, 25, 35), Continents.AF),
+		CENTRAL_AF(23, "Central Africa", List.of(20, 22, 24), Continents.AF),
+		SOUTH_AF(24, "South Africa", List.of(22, 23, 25), Continents.AF),
+		MADAGASCAR(25, "Madagascar", List.of(22, 24), Continents.AF),
 		
 		// Asia
-		URAL(26, "Ural",List.of(15,27,33,34), Continents.AS),
-		SIBERIA(27, "Siberia",List.of(26,28,30,31,34), Continents.AS),
-		YAKUTSK(28, "Yakutsk",List.of(27,29,30), Continents.AS),
-		KAMCHATKA(29, "Kamchatka",List.of(0,28,30,31,32), Continents.AS),
-		IRKUTSK(30, "Irkutsk",List.of(27,28,29,31), Continents.AS),
-		MONGOLIA(31, "Mongolia",List.of(27,29,30,32,34), Continents.AS),
-		JAPAN(32, "Japan",List.of(29,31), Continents.AS),
-		AFGHANISTAN(33, "Afghanistan",List.of(15,26,34,35,36), Continents.AS),
-		CHINA(34, "China",List.of(26,27,31,33,36,37), Continents.AS),
-		MIDDLE_EAST(35, "Middle East",List.of(15,19,21,22,33,36), Continents.AS),
-		INDIA(36, "India",List.of(33,34,35,37), Continents.AS),
-		SOUTHERN_AS(37, "Southern Asia",List.of(34,36,38), Continents.AS),
+		URAL(26, "Ural", List.of(15, 27, 33, 34), Continents.AS),
+		SIBERIA(27, "Siberia", List.of(26, 28, 30, 31, 34), Continents.AS),
+		YAKUTSK(28, "Yakutsk", List.of(27, 29, 30), Continents.AS),
+		KAMCHATKA(29, "Kamchatka", List.of(0, 28, 30, 31, 32), Continents.AS),
+		IRKUTSK(30, "Irkutsk", List.of(27, 28, 29, 31), Continents.AS),
+		MONGOLIA(31, "Mongolia", List.of(27, 29, 30, 32, 34), Continents.AS),
+		JAPAN(32, "Japan", List.of(29, 31), Continents.AS),
+		AFGHANISTAN(33, "Afghanistan", List.of(15, 26, 34, 35, 36), Continents.AS),
+		CHINA(34, "China", List.of(26, 27, 31, 33, 36, 37), Continents.AS),
+		MIDDLE_EAST(35, "Middle East", List.of(15, 19, 21, 22, 33, 36), Continents.AS),
+		INDIA(36, "India", List.of(33, 34, 35, 37), Continents.AS),
+		SOUTHERN_AS(37, "Southern Asia", List.of(34, 36, 38), Continents.AS),
 		
 		// Australia
-		INDONESIA(38, "Indonesia",List.of(37,39,40), Continents.AU),
-		NEW_GUINEA(39, "New Guinea",List.of(38,40,41), Continents.AU),
-		WESTERN_AU(40, "Western Australia",List.of(38,39,41), Continents.AU),
-		EASTERN_AU(41, "Eastern Australia",List.of(39,40), Continents.AU);
-		
+		INDONESIA(38, "Indonesia", List.of(37, 39, 40), Continents.AU),
+		NEW_GUINEA(39, "New Guinea", List.of(38, 40, 41), Continents.AU),
+		WESTERN_AU(40, "Western Australia", List.of(38, 39, 41), Continents.AU),
+		EASTERN_AU(41, "Eastern Australia", List.of(39, 40), Continents.AU);
 		
 		public final int           ID;
 		public final String        NAME;
