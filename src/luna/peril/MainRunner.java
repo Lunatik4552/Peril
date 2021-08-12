b024850a9c4d853736c2ad81a199c816c5d17015
Eric Rodriguez <ericrod.1998@gmail.com>
2021-08-11 22:11:50 -0700

    Added code to allow the board to add components to the panel

--- src/luna/peril/MainRunner.java
+++ src/luna/peril/MainRunner.java
@@ -2,7 +2,6 @@ package luna.peril;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.ArrayList;
 
 /**
  * <p></p>
@@ -13,52 +12,59 @@ import java.util.ArrayList;
 public class MainRunner extends JPanel
 {
 //	public Map map = new Map();
+	private Board board;
 	
 	MainRunner()
 	{
-		init();
+		
 	}
 	
 	public static void main(String[] args)
 	{
 		// Init the two frame and panel
 		JFrame mainFrame = new JFrame("Peril");
-		JPanel panel = new MainRunner();
-		panel.setPreferredSize(new Dimension(1000, 600));
+		MainRunner panel = new MainRunner();
+		panel.setPreferredSize(new Dimension(1600, 800));
 		
 		try
 		{
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-			System.out.println(UIManager.getSystemLookAndFeelClassName());
 		}
 		catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e)
 		{
 			e.printStackTrace();
 		}
+		
 		mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		mainFrame.setLayout(null);
 		mainFrame.setResizable(false);
 		mainFrame.setContentPane(panel);
 		mainFrame.pack();
+		panel.init();
 		mainFrame.setLocationRelativeTo(null);
 		mainFrame.setVisible(true);
 	}
 	
-	private void init()
+	void init()
 	{
-		
+		SpringLayout springLayout = new SpringLayout();
+		setLayout(springLayout);
+		board = new Board(this);
 	}
 	
 	public void paintComponent(Graphics g)
 	{
 		super.paintComponent(g);
 		
+		/*
 		g.setColor(Color.GREEN);
 		int[] xpoints = {160, 200, 300};
 		int[] ypoints = {200, 100, 150};
 		int npoints = 3;
 		
 		g.fillPolygon(xpoints, ypoints, npoints); //Venezuela Example
+		 */
+		
+		board.draw(g);
 	}
 	
 	/*
